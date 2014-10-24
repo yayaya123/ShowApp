786b051d24d33fb2a44e7b6548b338f57f592fea
yayaya123 <yann.leher@gmail.com>
2014-10-24 12:53:28 +0200

    avec des options pour les deux slideshows

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,16 +1,21 @@
 
   $(document).ready(function(){
     $("#slideshow").cycle({
-      fx: "scrollDown",
-      //easing: "easeOutÒBounce", //utilise le plugin easing.js
+      fx: "scrollDown", 
+      //easing: "easeOutÒBounce",
+      random: 1,
+      delay: -200,
+      timeout: 1000,
+      speed:200
       
     });
   }); 
 
   $(document).ready(function(){
     $("#test").cycle({
-      fx: "scrollDown",
+      fx: "fade",
       //easing: "easeOutÒBounce", //utilise le plugin easing.js
+      speed: 1200
       
     });
   }); 
